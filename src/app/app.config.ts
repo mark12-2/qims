9ac6718043b20b8be2125ee68bc7257154b35569
Christian Montesor <124326083+fads122@users.noreply.github.com>
2025-02-06 10:23:54 +0800

    sidebar changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,11 @@ import { importProvidersFrom } from '@angular/core';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 
 export const appConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    importProvidersFrom(BrowserModule, FormsModule), provideAnimationsAsync()
   ],
 };
--- src/app/pages/project-materials/project-materials.component.ts
+++ src/app/pages/project-materials/project-materials.component.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 const SUPABASE_URL = 'https://yvvuuiflqwppvezjmdsr.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2dnV1aWZscXdwcHZlemptZHNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2MzQ4MTksImV4cCI6MjA1NDIxMDgxOX0.9OZp412dfD1fdCqXTM8egs-M_iA9OVfwae_LrmB4rvo'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2dnV1aWZscXdwcHZlemptZHNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2MzQ4MTksImV4cCI6MjA1NDIxMDgxOX0.9OZp412dfD1fdCqXTM8egs-M_iA9OVfwae_LrmB4rvo';
 
 @Component({
   selector: 'app-project-materials',
@@ -45,10 +45,13 @@ export class ProjectMaterialsComponent {
   }
 
   openModal() {
+    console.log('Opening modal');
     this.showModal = true;
+    this.resetForm();
   }
 
   closeModal() {
+    console.log('Closing modal');
     this.showModal = false;
   }
 
